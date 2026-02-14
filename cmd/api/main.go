c7bf071cd6a13c7b42b3182da04fb37bb9359578
fanyicharllson <fanyicharllson1@gmail.com>
2026-02-13 17:11:42 -0800

    feat: Add database connection and storage structure for posts and users

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	db2 "github.com/fanyicharllson/backend-social/internal/db"
 	"github.com/fanyicharllson/backend-social/internal/env"
+	store2 "github.com/fanyicharllson/backend-social/internal/store"
 	"github.com/joho/godotenv"
 )
 
@@ -14,9 +17,36 @@ func main() {
 	}
 	cfg := config{
 		addr: env.GetString("SERVER_ADDR", ":8081"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
+	db, err := db2.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Panic(err)
+		}
+	}(db)
+	log.Println("Database connection pool established!")
+
+	store := store2.NewStorage(db)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
